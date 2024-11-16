6578973762d8b8f64cdaeb8aed41c9c95ad3eb7e
Ricepandaaaa <anthony.ha.pham@gmail.com>
2024-11-16 08:35:42 -0600

    First draft of book graphs

--- network.py
+++ network.py
@@ -1,13 +1,13 @@
 from typing import List
 from pyvis.network import Network
+import os
 
-def create_network(main_term: str, sub_terms: List[str], file_name: str):
+def create_network(terms: dict[str, List[str]], file_name: str):
     """ 
     Creates a network graph centered on the main term as a HTML file
     
     Args:
-        main_term (str): Name of the main term
-        sub_terms (List[str]): List of names of the associated terms
+        terms (dict[str, List[str]]): Dictionary of main terms and their connected terms
         file_name (str): Name of file to store network in
 
     Returns:
@@ -16,13 +16,42 @@ def create_network(main_term: str, sub_terms: List[str], file_name: str):
 
     # Set up base net
     net = Network()
-    net.add_node(n_id=main_term, label=main_term, color="#ff5733")
 
-    # Join the terms together
-    for sub_term in sub_terms:
-        net.add_node(n_id=sub_term, label=sub_term, color="#33cfff")
-        net.add_edge(main_term, sub_term)
+    for main_term in terms:
+        # Create new center node
+        net.add_node(n_id=main_term, label=main_term, color="#ff5733")
+        sub_terms = terms[main_term]
+
+        # Join the terms together
+        for sub_term in sub_terms:
+            net.add_node(n_id=sub_term, label=sub_term, color="#33cfff")
+            net.add_edge(main_term, sub_term)
 
     # Create the HTML file
-    net.write_html(file_name)
+    net.write_html(f"networks/{file_name}.html")
 
+
+# For generating the graphs
+if __name__ == "__main__":
+    # Open the relationships folder
+    for file_name in os.listdir("relationships/"):
+        file_path = os.path.join("relationships/", file_name)
+
+        with open(file_path, "r") as dataFile:
+            # Skip the header line
+            dataFile.readline()
+
+            # Extract the main term and the sub terms
+            terms = {}
+
+            for line in dataFile:
+                main_term, sub_term = line.split(",")
+                
+                if main_term not in terms:
+                    terms[main_term] = []
+                
+                terms[main_term].append(sub_term)
+
+            # Generate the HTML file with the network
+            file_start = file_name.split(".")[0]
+            create_network(terms, file_start)
